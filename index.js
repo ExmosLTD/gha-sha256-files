4be98f003700421d99dfbd824812a3c470c0d338
stuarts-exmos <86110417+stuarts-exmos@users.noreply.github.com>
2021-08-30 19:51:19 +0100

    Trying to fix imports
    Changing to const rather than ES6 imports

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import { setFailed } from '@actions/core';
-import github from '@actions/github';
-import fs from 'fs';
-import path from 'path';
-import fetch from 'node-fetch';
-import hasha from 'hasha';
-
+const fs = require("fs");
+const path = require("path");
+const core = require('@actions/core');
+const fs = require('fs');
+const hasha = require('hasha');
 
 try {
     var hashes = {};
     const files = core.getInput("files");
-
     files.split(" ").forEach(fileName => {
         // for each file we want to sha256 it.
         // read file
         // sha it with hasha and sha256
         const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
-
         if (!fs.existsSync(filePath)) {
             console.log(` - ${fileName} (Not Found)`);
             return;
         }
-
         hashes[fileName] = hasha(fs.readFileSync(filePath), {algorithm: "sha256"});
     })
     var result = "";
@@ -29,9 +24,6 @@ try {
     }
     core.setOutput("hashes", result);
     fs.writeFileSync("released-hashes.txt", result, {encoding: "utf8"});
-
 } catch(error) {
     setFailed(error.message);
 }
-
-//fs.writeFileSync(core.getInput("file-name"), result, {encoding: "ascii"});
