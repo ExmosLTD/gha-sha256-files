8f690b1a443b90c968b9aff184384f721d06ed18
stuarts-exmos <86110417+stuarts-exmos@users.noreply.github.com>
2021-08-31 21:00:09 +0100

    Recursively hash folders

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const core = require('@actions/core');
-const fs = require("fs");
-const path = require("path");
-const hasha = require('hasha');
-const files = core.getInput("files");
+import { getInput, setOutput } from '@actions/core';
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import hasha from 'hasha';
+const files = getInput("files");
+
+const toHashString = (f) => `${path.basename(f).padEnd(30, ' ')} ${hasha(fs.readFileSync(f), {algorithm: "sha256"})}`;
+
+function getAllFilesInFolderRecursively(inputFiles) {
+     return inputFiles.flatMap(file => 
+         fs.statSync(file).isDirectory()
+            ? getAllFilesInFolderRecursively(fs.readdirSync(file).map(f => path.join(file, f)))
+            : fs.realpathSync(file));
+}
 
 try {
-    var hashes = {};   
-    files.split(" ").forEach(fileName => {
-        const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
-        if (!fs.existsSync(filePath)) {
-            console.log(` - ${fileName} (Not Found)`);
-            return;
-        }
-        hashes[fileName] = hasha(fs.readFileSync(filePath), {algorithm: "sha256"});
-    })
-    var result = "";
-    for(const k in hashes) {
-        result += hashes[k] + " " + k + "\n";
-    }
-    core.setOutput("hashes", result);
-    fs.writeFileSync("released-hashes.txt", result, {encoding: "utf8"});
+    const filesToHash = files.split(" ").map(f => join(process.env.GITHUB_WORKSPACE, f));
+    var hashResult = getAllFilesInFolderRecursively(filesToHash).map(toHashString).join("\n");
+    setOutput("hashes", hashResult);
+    writeFileSync("released-hashes.txt", hashResult, {encoding: "utf8"});
 } catch(error) {
-    setFailed(error.message);
+    setFailed(error.message);    
 }
